b78a3936cd791881cf2d432c0bccc0d8ce1a2ef2
Shilu <shilu842003@gmail.com>
2023-11-11 21:33:16 +0530

    added main training function for maml

---
+++ main.py
@@ -0,0 +1,113 @@
+import argparse
+import os
+
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch import autograd  # pylint: disable=unused-import
+from torch.utils import tensorboard
+from dataloader import get_omniglot_dataloader
+from maml import MAML
+from utils import score 
+
+
+def main(args):
+    log_dir = args.log_dir
+    if log_dir is None:
+        log_dir = f'./logs/maml/omniglot.way:{args.num_way}.support:{args.num_support}.query:{args.num_query}.inner_steps:{args.num_inner_steps}.inner_lr:{args.inner_lr}.learn_inner_lrs:{args.learn_inner_lrs}.outer_lr:{args.outer_lr}.batch_size:{args.batch_size}'  # pylint: disable=line-too-long
+    print(f'log_dir: {log_dir}')
+    writer = tensorboard.SummaryWriter(log_dir=log_dir)
+
+    maml = MAML(
+        args.num_way,
+        args.num_inner_steps,
+        args.inner_lr,
+        args.learn_inner_lrs,
+        args.outer_lr,
+        log_dir
+    )
+
+    if args.checkpoint_step > -1:
+        maml.load(args.checkpoint_step)
+    else:
+        print('Checkpoint loading skipped.')
+
+    if not args.test:
+        num_training_tasks = args.batch_size * (args.num_train_iterations -
+                                                args.checkpoint_step - 1)
+        print(
+            f'Training on {num_training_tasks} tasks with composition: '
+            f'num_way={args.num_way}, '
+            f'num_support={args.num_support}, '
+            f'num_query={args.num_query}'
+        )
+        dataloader_train = get_omniglot_dataloader(
+            'train',
+            args.batch_size,
+            args.num_way,
+            args.num_support,
+            args.num_query,
+            num_training_tasks
+        )
+        dataloader_val = get_omniglot_dataloader(
+            'val',
+            args.batch_size,
+            args.num_way,
+            args.num_support,
+            args.num_query,
+            args.batch_size * 4
+        )
+        maml.train(
+            dataloader_train,
+            dataloader_val,
+            writer
+        )
+    else:
+        print(
+            f'Testing on tasks with composition '
+            f'num_way={args.num_way}, '
+            f'num_support={args.num_support}, '
+            f'num_query={args.num_query}'
+        )
+        dataloader_test = get_omniglot_dataloader(
+            'test',
+            1,
+            args.num_way,
+            args.num_support,
+            args.num_query,
+            NUM_TEST_TASKS
+        )
+        maml.test(dataloader_test)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Train a MAML!')
+    parser.add_argument('--log_dir', type=str, default=None,
+                        help='directory to save to or load from')
+    parser.add_argument('--num_way', type=int, default=5,
+                        help='number of classes in a task')
+    parser.add_argument('--num_support', type=int, default=1,
+                        help='number of support examples per class in a task')
+    parser.add_argument('--num_query', type=int, default=15,
+                        help='number of query examples per class in a task')
+    parser.add_argument('--num_inner_steps', type=int, default=1,
+                        help='number of inner-loop updates')
+    parser.add_argument('--inner_lr', type=float, default=0.4,
+                        help='inner-loop learning rate initialization')
+    parser.add_argument('--learn_inner_lrs', default=False, action='store_true',
+                        help='whether to optimize inner-loop learning rates')
+    parser.add_argument('--outer_lr', type=float, default=0.001,
+                        help='outer-loop learning rate')
+    parser.add_argument('--batch_size', type=int, default=16,
+                        help='number of tasks per outer-loop update')
+    parser.add_argument('--num_train_iterations', type=int, default=15000,
+                        help='number of outer-loop updates to train for')
+    parser.add_argument('--test', default=False, action='store_true',
+                        help='train or test')
+    parser.add_argument('--checkpoint_step', type=int, default=-1,
+                        help=('checkpoint iteration to load for resuming '
+                              'training, or for evaluation (-1 is ignored)'))
+
+    main_args = parser.parse_args()
+    main(main_args)
